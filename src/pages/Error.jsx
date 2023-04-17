c9877257074ccc248c1aa6135252a636b09f49a3
Stéphane Morel <110528810+Steph4477@users.noreply.github.com>
2023-04-17 11:40:45 +0200

    implementation of the error page with its style

--- src/pages/Error.jsx
+++ src/pages/Error.jsx
@@ -1,13 +1,26 @@
-import ErrorComp from '../components/Error'
+import { Link } from 'react-router-dom'
 
 export default function Error() {
 
     return (
-        
-        <main>
-            <ErrorComp />
-        </main>
+        <section className='error'>
+            
+            <div className='error_number'>
+                404
+            </div>
+            
+            <div className='error_msg'>
+                Oups! La page que vous demandez n'existe pas.
+            </div>
+            
+            <Link className="error_linkHome"
+                to="/"
+               >Retour à la page d'accueil
+            </Link>
+
+        </section>
+
 
     )
-    
+
 }
