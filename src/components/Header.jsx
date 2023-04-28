104a19412ddfab273f720ee2d63a5392d327c5bf
Stéphane Morel <110528810+Steph4477@users.noreply.github.com>
2023-04-28 13:27:55 +0200

    footer full width and underline navbar active

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -1,36 +1,44 @@
-import Logo from '../assets/logo.svg'
-import { Link } from 'react-router-dom'
+import Logo from '../assets/logo.svg';
+import { useState } from 'react';
+import { Link } from 'react-router-dom';
 
 export default function Header() {
+    const [activeTab, setActiveTab] = useState('Accueil')
+
+    const tabClick = (tab) => {
+        setActiveTab(tab)
+    }
 
     return (
-
-        <header className='header'>
-            
-            <Link to='/'>
-                <img className='header_logo' src={Logo} alt="logo de l'agence Kasa" />
+        <header className="header">
+            <Link to="/">
+                <img className="header_logo" src={Logo} alt="logo de l'agence Kasa" 
+                onClick={() => tabClick('Accueil')} />
             </Link>
 
-            <nav className='header_nav'>
-                <ul className='header_nav_onglets'>
-
+            <nav className="header_nav">
+                <ul className="header_nav_onglets">
                     <li>
-                        <Link to='/' className='header_nav_onglets_onglet'>
+                        <Link
+                            to="/"
+                            className={`header_nav_onglets_onglet ${activeTab === 'Accueil' ? 'active' : '' }`}
+                            onClick={() => tabClick('Accueil')}
+                        >
                             Accueil
                         </Link>
                     </li>
-
                     <li>
-                        <Link to='/about' className='header_nav_onglets_onglet'>
+                        <Link
+                            to="/about"
+                            className={`header_nav_onglets_onglet ${activeTab === 'A Propos' ? 'active' : '' }`}
+                            onClick={() => tabClick('A Propos')}
+                        >
                             A Propos
                         </Link>
                     </li>
-
                 </ul>
             </nav>
-            
         </header>
     )
 }
-
 
